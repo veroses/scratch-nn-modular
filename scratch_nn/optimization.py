145b5aa2926df55862814636bbcc73a6583f8436
verose <owen.lee2406@gmail.com>
2025-07-03 19:07:39 -0700

    added layer_idx paramter to update

--- scratch_nn/optimization.py
+++ scratch_nn/optimization.py
@@ -24,13 +24,14 @@ class SGD(Optimizer):
         self.momentum = momentum
         self.velocity = {}
 
-    def update(self, params, grads):
-        for key in params:
+    def update(self, params, grads, layer_idx):
+        for p_key in params:
+            key = f"{layer_idx}_{p_key}"
             if key not in self.velocity:
-                self.velocity[key] = np.zeros_like(params[key])
+                self.velocity[key] = np.zeros_like(params[p_key])
 
-            self.velocity[key] = self.momentum * self.velocity[key] - self.learning_rate * grads[key]
-            params[key] += self.velocity[key]
+            self.velocity[key] = self.momentum * self.velocity[key] - self.learning_rate * grads[p_key]
+            params[p_key] += self.velocity[key]
 
 class Adam(Optimizer):
     def __init__(self, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
@@ -43,20 +44,21 @@ class Adam(Optimizer):
         self.t = 0
 
 
-    def update(self, params, grads):
+    def update(self, params, grads, layer_idx):
         self.t += 1
-        for key in params:
+        for p_key in params:
+            key = f"{layer_idx}_{p_key}"
             if key not in self.moment:
-                self.moment[key] = np.zeros_like(params[key])
-                self.velocity[key] = np.zeros_like(params[key])
+                self.moment[key] = np.zeros_like(params[p_key])
+                self.velocity[key] = np.zeros_like(params[p_key])
 
-            self.moment[key] = self.beta1 * self.moment[key] + (1 -  self.beta1) * grads[key]
-            self.velocity[key] = self.beta2 * self.velocity[key] + (1 - self.beta2) * np.square(grads[key])
+            self.moment[key] = self.beta1 * self.moment[key] + (1 -  self.beta1) * grads[p_key]
+            self.velocity[key] = self.beta2 * self.velocity[key] + (1 - self.beta2) * np.square(grads[p_key])
 
             moment_corr = self.moment[key] / (1 - self.beta1 ** self.t)
             velocity_corr = self.velocity[key] / (1 - self.beta2 ** self.t)
 
-            params[key] -= self.learning_rate * moment_corr / (np.sqrt(velocity_corr) + self.epsilon)  
+            params[p_key] -= self.learning_rate * moment_corr / (np.sqrt(velocity_corr) + self.epsilon)  
 
 
 def get_optimizer(name, **kwargs):
